e0994160533a161229d2500adebb68841833b527
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-25 22:33:27 -0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -37,6 +37,7 @@ class _BotCommands:
         self.BtSelectCommand = f'btsel{CMD_SUFFIX}'
         self.RssCommand = f'rss{CMD_SUFFIX}'
         self.CategorySelect = f'catsel{CMD_SUFFIX}'
+        self.BroadcastCommand = [f'broadcast{CMD_SUFFIX}', f'bc{CMD_SUFFIX}']
         self.RmdbCommand = f'rmdb{CMD_SUFFIX}'
 
 BotCommands = _BotCommands()
